88e22ccba43f683e9660b63b25ba16f53ea458e1
maxim <max@myplaza.com>
2024-06-26 21:40:14 +0300

    Добавьте файлы проекта.

---
+++ Watchit/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Watchit.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
